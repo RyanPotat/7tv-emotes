2f5d8c86ac624f6a05d16078067d165616620701
MarkZynk <markzynk@gmail.com>
2023-10-08 23:19:00 -0400

    Fixed 7tvAdd command

--- src/handler/CommandHandler.ts
+++ src/handler/CommandHandler.ts
@@ -2,14 +2,14 @@ import type { PrivmsgMessage } from '@kararty/dank-twitch-irc';
 import { ChannelHandler } from './ChannelHandler.js';
 
 export async function CommandHandler(msg: PrivmsgMessage) {
-	const { channelName, channelID, senderUserID, messageText } = msg;
+	const { channelID, senderUserID, messageText } = msg;
 	if (!Bot.Config.Admins.includes(channelID) || !Bot.Config.Admins.includes(senderUserID)) return;
 
 	const prefix = '!';
 	const args = messageText.slice(prefix.length).trim().split(/ +/g);
 	const cmd = args.length > 0 ? args.shift()!.toLowerCase() : '';
 
-	if (cmd === '7tvAdd' && args[0]) {
+	if (cmd === '7tvadd' && args[0]) {
 		await ChannelHandler(args[0]);
 	}
 }
